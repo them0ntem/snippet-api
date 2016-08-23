ba92b66498832520118c3c4ba78bb179750e821e
Manthan Sharma <hitechmanthan@gmail.com>
2016-08-24 02:16:26 +0530

    Update auth_token url
    Signed-off-by: Manthan Sharma <hitechmanthan@gmail.com>

--- snippet/urls.py
+++ snippet/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
 	url(r'^snippet/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 	url(r'^users/$', views.UserList.as_view(), name='user-list'),
 	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-	url(r'^snippet-token-auth/', views.ObtainAuthToken.as_view())
+	url(r'^api-token-auth/', views.ObtainAuthToken.as_view())
 
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
 # Login and logout views for the browsable API
 urlpatterns += [
-	url(r'^snippet-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
